6b5e5a491602eb020478fa1216af7b0aaa427a2b
krok101g <krok101g@gmail.com>
2022-04-09 13:07:33 +0400

    feat: add progress line

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -7,6 +7,7 @@ const Player = () => {
   const [isPlay, setIsPlay] = useState(false);
   const [isFullscreen, setFullscreen] = useState(false);
   const [theaterMode, setTheaterMode] = useState(false);
+  const [progress, setProgerss] = useState('0%');
   const configVideo = useSelector(state => state.configVideo);
 
   const video = useRef(null);
@@ -41,16 +42,22 @@ const Player = () => {
     setIsPlay(false);
   }
 
+  const onTimeUpdate = (event) => {
+    const {currentTime, duration} = event.target
+    setProgerss(`${currentTime / duration * 100 }%`)
+  }
+
   return (
     <div className={`${style.player} ${theaterMode ? style.theaterMode: null}`}> 
       <div className={style.videoContainer}>
-        <video ref={video} width='100%' onEnded={onEndedVideo}></video>
+        <video ref={video} width='100%' onEnded={onEndedVideo} onTimeUpdate={onTimeUpdate} />
         <h2 className={style.videoLabel}>{configVideo.name}</h2>
       </div>
       <Controls
         dataPlay = {{state: isPlay, action: clickOnPlayPause}}
         dataFullscreen = {{state: isFullscreen, action: clickOnFullScreen}}
         dataTheaterMode = {{state: theaterMode, action: clickOnTheaterMode}}
+        progress={progress}
       />
     </div>
   );
